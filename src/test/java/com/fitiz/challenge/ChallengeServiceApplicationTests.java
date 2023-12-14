f4ae5e65e617b1b06ba46fb30904f67ccd8ab107
burakemreozer <burak.oezer@checktur.io>
2023-12-14 04:11:48 +0100

    init repo

---
+++ src/test/java/com/fitiz/challenge/ChallengeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitiz.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
