aa5f8973e2aec47ecff7afeee799f287bb7c8770
burakemreozer <burak.oezer@checktur.io>
2024-01-06 23:03:48 +0100

    Debug workflow

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://codedeploystack-webappdeploymentbucket-hre3kakybzrj/ChallengeServiceApplication.war /usr/local/tomcat9/webapps/ChallengeServiceApplication.war
+aws s3 cp s3://codedeploystack-webappdeploymentbucket-hre3kakybzrj/challenge-service-0.0.1-SNAPSHOT.war /usr/local/tomcat9/webapps/challenge-service-0.0.1-SNAPSHOT.war
 
 
 # Ensure the ownership permissions are correct.
