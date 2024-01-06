083880bd8483128ce1abbddffaf890439e3948fc
burakemreozer <burak.oezer@checktur.io>
2024-01-06 23:20:31 +0100

    Debug workflow

---
+++ src/main/java/com/fitiz/challenge/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.fitiz.challenge.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/")
+    public String hello() {
+        return "<h1> Congratulations. " +
+                "You have successfully deployed the sample Spring Boot Application. " +
+                "</h1>";
+    }
+}
