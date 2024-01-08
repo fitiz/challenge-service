996356b0867c1ad3ab16f5e0733fef70ffb97fc7
burakemreozer <burak.oezer@checktur.io>
2024-01-08 02:45:42 +0100

    Debug workflow

--- src/main/java/com/fitiz/challenge/controller/HelloWorldController.java
+++ src/main/java/com/fitiz/challenge/controller/HelloWorldController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @RequestMapping("/")
     public String hello() {
-        return "<h1> Congratulations. " +
-                "You have successfully deployed the sample Spring Boot Application. " +
-                "</h1>";
+        return "Hello world!";
     }
 }
