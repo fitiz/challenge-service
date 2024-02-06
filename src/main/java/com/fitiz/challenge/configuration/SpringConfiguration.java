32df02cb53637e37bd73c3a4462e3c67cfd4a41c
burakemreozer <burakemreozer@gmail.com>
2024-02-06 01:36:18 +0100

    FIT-4: Configs for Kafka and Redis

---
+++ src/main/java/com/fitiz/challenge/configuration/SpringConfiguration.java
@@ -0,0 +1,12 @@
+package com.fitiz.challenge.configuration;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+public class SpringConfiguration {
+
+}
